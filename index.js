53b9e16b365f21e3fb87a581f3dfdcce4f0ba7bf
saikishorereddy533 <84308565+saikishorereddy533@users.noreply.github.com>
2023-01-21 16:05:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true })); 
 
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/",userRoute);
